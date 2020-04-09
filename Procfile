1e213c698d9c96e98d4854a1ee58715a236e2292
Sagar Banik <sagarbanik11@gmail.com>
2020-04-09 21:38:15 +0530

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn songofy.wsgi --log-file -
--- songofy/settings.py
+++ songofy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+g-$mr@to63%zyj_*$#w-9dmz^7ghp0(97ea8&y48$gl1mj&&p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
